c7594cc2bf6a8362c82cca09d7099a5fcb2f85b6
Şeyma Gündoğdu <symagn2@gmail.com>
2022-10-28 11:01:25 +0300

    props , proptypes, defaultprops

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import User from "./react-101//components/User";
 
+// const user = {
+//   name:"Şeyma",
+//    surname:"Gündoğdu",
+//     age:"23"
+// }
+
 function App() {
   return (
     <>
-    <User name="Şeyma" surname="Gündoğdu" age= "23"/>
+    <User title="User"
+    name="Şeyma"
+    city="Zonguldak"
+    age={29}
+    />
+    {/*  friends={["Ahmet" , "Murat" , "Ayşe" , "Fatma"]}  */}
     </>
  
   );
--- src/react-101/components/User.js
+++ src/react-101/components/User.js
@@ -1,17 +1,35 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function User(  ifade) {
+function User({title,name,age,city}) {
  
+
+
   return (
     
     <div>
-      User
-      <div>İsim: {ifade.name}</div>
-      <div>Yaş: {ifade.surname}</div>
-      <div>Yaş: {ifade.age}</div>
+    <strong>{title}</strong>
+      <div>Name: {name}</div>
+      <div>Age: {age}</div>
+      <div>City: {city}</div>
+
       <hr/>
+      {/* <h3>Arkadaşlar</h3>
+      {
+        friends.map((friend,id)  => <div key={id} >{friend}</div>)
+      }
+      <hr/> */}
     </div>
   );
 }
 
+User.propTypes = {
+  title: PropTypes.string,
+  name: PropTypes.string,
+   age: PropTypes.number,
+}
+User.defaultProps = {
+  
+  title:"User"
+}
 export default User;
