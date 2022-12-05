1f8934fccd3c00a03acaa5860587abbfeefb67a0
seymagundogduu <symagn2@gmail.com>
2022-12-05 18:54:35 +0300

    fetch & axios

--- src/react-201/components/Users.js
+++ src/react-201/components/Users.js
@@ -4,24 +4,24 @@ import React, {useEffect, useState} from 'react'
 function Users() {
     const [users, setUsers] = useState([]);
     const [loading, setLoading] = useState(true);
-    // useEffect(() => {
-    //     fetch("https://jsonplaceholder.typicode.com/users")
-    //     .then(res => res.json())
-    //     .then(data => setUsers(data))
-    //     .finally(() => setLoading(false))
-    // },[]) 
-
     useEffect(() => {
-        axios("https://jsonplaceholder.typicode.com/users")
-            .then((res) => setUsers(res.data))
-            .finally(() => setLoading(false))
-    })
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res => res.json())
+        .then(data => setUsers(data))
+        .finally(() => setLoading(false))
+    },[]) 
+
+    // useEffect(() => {
+    //     axios("https://jsonplaceholder.typicode.com/users")
+    //         .then((res) => setUsers(res.data))
+    //         .finally(() => setLoading(false))
+    // })
   return (
     <div> Users 
 
         {loading && <div>Yükleniyor....</div> } 
       <ul>   
-           { users.map((user) => ( <li key={user.id}> {user.name} {user.username} {user.email} </li>) )}
+           { users.map((user) => ( <li key={user.id}> {user.name} {user.username} {user.email}  </li>) )}
     
       </ul>
     </div>
