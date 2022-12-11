be95591e75d9a8154fc839f3e889d947cd6ee434
seymagundogduu <symagn2@gmail.com>
2022-12-11 19:44:15 +0300

    formik

--- src/react-201/routing/pages/Dashboard/Contact.js
+++ src/react-201/routing/pages/Dashboard/Contact.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Formik, Field, Form } from 'formik';
 
 function Contact() {
   return (
     <div>
         <h2>Contact</h2>
+        <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+      }}
+      onSubmit={ (values) => {
+      
+        console.log(JSON.stringify(values));
+      }}
+    >
+      <Form>
+        <label htmlFor="firstName">First Name</label>
+        <Field id="firstName" name="firstName" placeholder="Jane" />
+
+        <label htmlFor="lastName">Last Name</label>
+        <Field id="lastName" name="lastName" placeholder="Doe" />
+
+        <label htmlFor="email">Email</label>
+        <Field
+          id="email"
+          name="email"
+          placeholder="jane@acme.com"
+          type="email"
+        />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
     </div>
   )
 }
