1cbfbd0a91093d40d77cf4ff55f77c761a1e761a
seymagundogduu <symagn2@gmail.com>
2022-12-11 11:29:18 +0300

    react-router-dom

--- src/react-201/routing/App.js
+++ src/react-201/routing/App.js
@@ -50,31 +50,3 @@ function App() {
 }
 
 export default App;
- 
-
-//react 101de kullanılan yapılar
-// const user = {
-//   name:"Şeyma",
-//    surname:"Gündoğdu",
-//     age:"23"
-// }
-
-// function App() {
-//   return (
-//     <>
-//     {/* <User title="User"
-//     name="Şeyma"
-//     city="Zonguldak"
-//     age={29}
-//     />
-//      friends={["Ahmet" , "Murat" , "Ayşe" , "Fatma"]}  */}
-
-//      <Counter/>
-//       <Form/>
-
-//      {/* <User_2/> */}
-//      {/* <Colors/> */}
-//     </>
- 
-//   );
-// }
---
+++ src/react-201/routing/layouts/Dashboard/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Menu from '../../components/Menu'
+
+function DashboardLayout() {
+   
+  return (
+ 
+    <div>
+           <Menu/>
+        
+        <div id='content'>
+        <Outlet/>
+        </div>
+        
+        </div>
+  )
+}
+
+export default DashboardLayout
