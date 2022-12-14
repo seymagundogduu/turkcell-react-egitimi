620d07cc470e071381b9b49f2b44ae58a7b31793
seymagundogduu <symagn2@gmail.com>
2022-12-14 21:08:54 +0300

    react-101 app.js

---
+++ src/react-101/components/App.js
@@ -0,0 +1,26 @@
+//react 101de kullanılan yapılar
+// const user = {
+//   name:"Şeyma",
+//    surname:"Gündoğdu",
+//     age:"23"
+// }
+
+// function App() {
+//   return (
+//     <>
+//     {/* <User title="User"
+//     name="Şeyma"
+//     city="Zonguldak"
+//     age={29}
+//     />
+//      friends={["Ahmet" , "Murat" , "Ayşe" , "Fatma"]}  */}
+
+//      <Counter/>
+//       <Form/>
+
+//      {/* <User_2/> */}
+//      {/* <Colors/> */}
+//     </>
+ 
+//   );
+// }
