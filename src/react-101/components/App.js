df39b238ff32cf629a793c1b6663f1f9274ed2b6
seymagundogduu <symagn2@gmail.com>
2022-12-14 21:09:23 +0300

    101 app.js

--- src/react-101/components/App.js
+++ src/react-101/components/App.js
@@ -1,26 +1,27 @@
-//react 101de kullanılan yapılar
-// const user = {
-//   name:"Şeyma",
-//    surname:"Gündoğdu",
-//     age:"23"
-// }
-
-// function App() {
-//   return (
-//     <>
-//     {/* <User title="User"
-//     name="Şeyma"
-//     city="Zonguldak"
-//     age={29}
-//     />
-//      friends={["Ahmet" , "Murat" , "Ayşe" , "Fatma"]}  */}
-
-//      <Counter/>
-//       <Form/>
-
-//      {/* <User_2/> */}
-//      {/* <Colors/> */}
-//     </>
  
-//   );
-// }
+ import React , {useState} from "react"
+
+ import "./App.css"
+
+ const user = {
+    surname:"Gündoğdu",
+        name:"Şeyma",
+   age:"23"
+}
+
+ function App() {
+   return (
+     <>
+     {/* <User title="User"
+     name="Şeyma"
+     city="Zonguldak"
+    age={29}
+     />
+     friends={["Ahmet" , "Murat" , "Ayşe" , "Fatma"]}  */}
+
+      {/* <User_2/> */}
+      {/* <Colors/> */}
+     </>
+ 
+   ); }
+export default App();
