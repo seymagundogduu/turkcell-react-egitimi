b134e87b9c2569933795f416f1a14f0985be7071
seymagundogduu <symagn2@gmail.com>
2022-12-19 18:03:39 +0300

    react-intl

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import React from "react";
-
+import React, {useState} from "react";
 import "./react-301/styles.css";
+import { IntlProvider, FormattedMessage, FormattedNumber } from "react-intl";
 
-import { LangContextProvider } from "./react-301/contexts/LangContext";
-
-import { ThemeContextProvider } from "./react-301/contexts/ThemeContext";
-import Container from "./react-301/Container";
-
-const App = () => (
-    < >
-      <LangContextProvider>
-      <ThemeContextProvider>
-      <Container/>
-      </ThemeContextProvider>
-      </LangContextProvider>
+function App() {
+  const [lang, setLang] = useState("tr-TR")
+  const messages = {
+   "tr-TR" : {
+    title: "Merhaba Dünya",
+    description: "3 yeni mesajınız var"
+   },
+   "en-US" : {
+    title: "Hello World",
+    description:"you have 3 new message"
+   }
+  }
+  return (
+    <div>
+      <IntlProvider messages={messages[lang]}>
+      <FormattedMessage id="title"/>
+   <br/>
+   <hr/>
+   <p> 
+    <FormattedMessage id="description" />
+   </p>
+   <hr/>
+      <button onClick={() => setLang('tr-TR')}>TR</button>
       
-    </>
+      <button onClick={() => setLang("en-US")}>EN</button>
+      </IntlProvider>
+    </div>
   );
-
+}
 
 export default App;
